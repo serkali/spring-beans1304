7b78a6ca86670f98eb2898d226ac7069b48582a0
serkali88 <serkali88@gmail.com>
2021-04-13 17:41:08 +0400

    Java pre-project. Практическая задача 2.1.3

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,7 +1,12 @@
 package app.model;
 
-public class Timer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier("dog")
+public class Timer {
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
