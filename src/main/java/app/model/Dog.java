b017d9933739a8b914ed1714b7b0fa4465d218c5
serkali88 <serkali88@gmail.com>
2021-05-16 23:13:00 +0400

    Java pre-project. Практическая задача 2.1.3

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,14 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("animal")
+public class Dog extends Animal{
+    @Override
+    public String toString(){
+        return "Im a Dog";
+    }
+}
